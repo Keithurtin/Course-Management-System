7eecf34addfb8b5294978aa69646556b7493792d
Keithurtin <keithurtin1604@gmail.com>
2023-03-17 13:33:52 +0700

    Task 1 finished

---
+++ Part_1/Task_01/schoolYear.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "schoolYear.h"
+
+using namespace std;
+
+void createSchoolYear(SchoolYear* &pHead)
+{
+	cin >> pHead->startYear >> pHead->endYear;
+	pHead->classroom = new Class;
+	Class* cur = pHead->classroom;
+	while (cin >> cur->name)
+	{
+		cur->student = nullptr;
+		if (cur->name == "0")
+		{
+			cur->pNext = nullptr;
+			return;
+		}
+		cur->pNext = new Class;
+		cur = cur->pNext;
+	}
+}
