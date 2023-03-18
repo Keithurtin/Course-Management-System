94324ee9064b3f952b282ad4bb947c5976fd4ba2
IaTOgN <taingotan0811@gmail.com>
2023-03-18 22:43:48 +0700

    Task 19 (Not tested yet)
    Did not have data to test so i didn't test it yet

---
+++ Part_3/Task19.cpp
@@ -0,0 +1,19 @@
+#include "header.h"
+
+void ExportListOfStudsInACourse(ofstream& ListOfStuds, Course Cour){
+    ListOfStuds.open("ListOfStudents.csv");
+    if(ListOfStuds.is_open() == false){
+        cout << "Error writing file. \n";
+        return;
+    }
+
+    ListOfStuds << "Course ID: " << Cour.courseID << endl;
+    ListOfStuds << "Course name: " << Cour.courseName << endl;
+    ListOfStuds << "Teacher's name: " << Cour.teacherName << endl;
+    ListOfStuds << "Student's ID, Gender, Name" << endl;
+    for(int i = 0; i < Cour.maxStudents; i++){
+        ListOfStuds << Cour.Studs[i].studentID << "," << Cour.Studs[i].gender << "," << Cour.Studs[i].fullname << endl;
+    }
+
+    ListOfStuds.close();
+}
