e962ccabb799746c0b7412036e1f40399e15a353
dungpq22clc02 <pqdung22@clc.fitus.edu.vn>
2023-04-10 12:42:08 +0700

    Update Task24_viewtranscript.cpp

--- Task24_viewtranscript.cpp
+++ Task24_viewtranscript.cpp
@@ -1,42 +1,48 @@
 #include"allStruct.h"
 
-void displayTranscript(Student* pHead, string studentID, Semester* pCurrentSemester, int semester, int year)
+void printStudentMarksInSemester(string studentID, SchoolYear* pHeadYear, int school_year, int semester_num)
 {
-    if (pCurrentSemester == nullptr)
+    // Find the school year node with the specified starting year
+    SchoolYear* pYear = pHeadYear;
+    while (pYear != nullptr && pYear->startYear != school_year)
     {
-        cout << "No current semester found." << endl;
-        return;
+        pYear = pYear->pNext;
     }
 
-    if (pCurrentSemester->school_year.year != year || pCurrentSemester->semester_num != semester)
+    // If the specified school year is not found, exit the function
+    if (pYear == nullptr)
     {
-        cout << "No courses found for semester " << semester << " of school year " << year << "-" << year + 1 << endl;
+        cout << "School year " << school_year << "-" << school_year + 1 << " not found." << endl;
         return;
     }
 
-    Student* curStudent = pHead;
-    while (curStudent != nullptr && curStudent->studentID != studentID) {
-        curStudent = curStudent->pNext;
+    // Find the semester node with the specified semester number
+    Semester* pSemester = pYear->semesters;
+    while (pSemester != nullptr && pSemester->semester_num != semester_num)
+    {
+        pSemester = pSemester->p_Next_Semester;
     }
 
-    if (curStudent == nullptr) {
-        cout << "Student not found." << endl;
+    // If the specified semester is not found, exit the function
+    if (pSemester == nullptr)
+    {
+        cout << "Semester " << semester_num << " not found in school year " << school_year << "-" << school_year + 1 << "." << endl;
         return;
     }
 
-    cout << "Transcript for student with ID " << studentID << " in semester " << pCurrentSemester->semester_num << " - School year: " << pCurrentSemester->school_year.year << " - " << pCurrentSemester->school_year.year + 1 << ":" << endl;
-    cout << "Full Name: " << curStudent->fullname << endl;
-    cout << "----------------------------------------------------------------------------------------------------------------------" << endl;
+    cout << "Transcript for student with ID " << studentID << " in semester " << semester_num << " - School year: " << school_year << " - " << school_year + 1 << ":" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
     cout << "| " << setw(12) << left << "Course ID"
         << "| " << setw(35) << left << "Course Name"
         << "| " << setw(10) << left << "Credits"
-        << "| " << setw(8) << left << "Midterm"
-        << "| " << setw(6) << left << "Other"
-        << "| " << setw(5) << left << "Final"
-        << "| " << setw(6) << left << "Total" << " |" << endl;
-    cout << "----------------------------------------------------------------------------------------------------------------------" << endl;
+        << "| " << setw(10) << left << "Midterm"
+        << "| " << setw(10) << left << "Other"
+        << "| " << setw(10) << left << "Final"
+        << "| " << setw(10) << left << "Total" << " |" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
 
-    Course* curCourse = pCurrentSemester->p_CourseList;
+    // Find the student node with the specified student ID in all courses in the semester
+    Course* curCourse = pSemester->p_CourseList;
     while (curCourse != nullptr)
     {
         Student* curStudentInCourse = curCourse->Studs;
@@ -47,11 +53,11 @@ void displayTranscript(Student* pHead, string studentID, Semester* pCurrentSemes
                 cout << "| " << setw(12) << left << curCourse->courseID
                     << "| " << setw(35) << left << curCourse->courseName
                     << "| " << setw(10) << left << curCourse->credits
-                    << "| " << setw(8) << left << curStudentInCourse->midtermMark
-                    << "| " << setw(6) << left << curStudentInCourse->otherMark
-                    << "| " << setw(5) << left << curStudentInCourse->finalMark
-                    << "| " << setw(6) << left << curStudentInCourse->totalMark << " |" << endl;
-                cout << "----------------------------------------------------------------------------------------------------------------------" << endl;
+                    << "| " << setw(10) << left << curStudentInCourse->midtermMark
+                    << "| " << setw(10) << left << curStudentInCourse->otherMark
+                    << "| " << setw(10) << left << curStudentInCourse->finalMark
+                    << "| " << setw(10) << left << curStudentInCourse->totalMark << " |" << endl;
+                cout << "-----------------------------------------------------------------------------------------------------------------" << endl;
             }
             curStudentInCourse = curStudentInCourse->pNext;
         }
